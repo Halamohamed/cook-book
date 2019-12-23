6f908a097c14709c483def652d006d3f8c3a1b1d
Hala Mohammed <halaalimohammed@gmail.com>
2019-12-23 13:18:32 +0100

    update menu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let headElement = document.getElementsByClassName('header');
-
+let childHome;
 let sectionElement = document.getElementsByTagName('section');
 let divElement = document.getElementsByTagName('div');
 let imageElement = document.getElementById('imageId');
@@ -16,13 +16,23 @@ function init(){
 
 
 function getStartSide(){
+    let navRef = document.getElementById('nav');
 
+    document.getElementById('nav');
+
+    let home = navRef.childNodes[1];
+     childHome = home.childNodes[1];
+
+    console.log(childHome);
+    childHome.addEventListener('click', function(){
+        childHome.setAttribute('href', 'chicken.html');
+    });
     
-    headElement.innerHTML = 'EC COOK BOOK';
     
+    //home.addEventListener('click', addList);
     
     //divElement.setAttribute('style', 'width:300px; height:600px');
-    let navRef = document.getElementById('nav');
+    
 
     //document.body.appendChild(sectionElement);
     //sectionElement.appendChild(divElement);
@@ -30,34 +40,15 @@ function getStartSide(){
     //divElement.appendChild(paragElement);
 
 }
+function addList(){
+    childHome.setAttribute('href','chicken.html');
+}
 
 function getImage(){
     imageElement.setAttribute('src', 'images/cucumber-salmon-panzanella-1565019143.jpg');
     imageElement.setAttribute('id', 'imageId');
 }
 
-var ingredint1 = 
-    ['8 oz. country bread, torn',
-    '2 tbsp. olive oil',
-    'Kosher salt and black pepper',
-    '4 Persian cucumbers',
-    '3 tbsp. red wine vinegar',
-    
-    '1 tsp. caraway seeds',
-    '1/4 red onion, sliced',
-    '1 small fennel bulb, sliced',
-    '1 c. arugula',
-    'Roasted salmon'];
-var ingredint2 = ['',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '']
 
     function addDescription(){
         var description1 = '\nDescription: \n';
